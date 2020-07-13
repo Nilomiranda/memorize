b7f5ceeb3b72f27574ddd83a0b24182361a53ede
Nilomiranda <nilomiranda3@gmail.com>
2020-07-13 20:50:36 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Danilo Miranda on 13/07/20.
+//  Copyright © 2020 Danilo Miranda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
