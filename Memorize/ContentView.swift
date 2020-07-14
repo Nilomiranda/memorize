15ed773b425743e6a27073b305cd8a57bbcbac60
Nilomiranda <nilomiranda3@gmail.com>
2020-07-14 16:48:09 -0300

    basic components and styling concepts

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,11 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 10.0)
-            Text("👻")
-        })
-        
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill().foregroundColor(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke().foregroundColor(Color.orange)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
